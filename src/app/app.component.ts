f1086850b8ac6175178fd118e114b81267f30e00
vikram-nunna <chinnivikramnunna@gmail.com>
2024-08-19 09:59:18 -0500

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,29 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'blog';
    catg='home';
+   toggle=true;
    catgchange(event:Event):void {
     const target = event.target as HTMLButtonElement;
     this.catg=target.value;
     console.log(this.catg);
+    
    }
+   changetoggle(event:Event):void
+    {
+      this.toggle=!this.toggle;
+    }
+    
+    
+    @HostListener('click', ['$event'])
+  onDivClick(event: MouseEvent) {
+    const targetElement = event.target as HTMLElement;
+
+    // Traverse up the DOM tree to ensure we are on the actual div
+    const clickableDiv = targetElement.closest('.clickable-div');
+    
+    if (clickableDiv) {
+      console.log('Clicked div ID:', clickableDiv.id);
+      this.toggle=true;
+    }
+  }
 }
